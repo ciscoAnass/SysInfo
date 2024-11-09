8554a1226fec37fd2fb56dc70ec939accbcef737
ciscoanass <hello@anass-assim.es>
2024-11-09 15:13:45 +0100

    Edit SysInfo English Script

--- SysInfo.sh
+++ SysInfo.sh
@@ -1,47 +1,42 @@
 #!/bin/bash
 clear
 
+# Display current system information
+echo "System Information - $(date)"
+echo "--------------------------------"
 
-#VARIABLES Disk
+# Disk Information
+DiskInfo=$(df -hT / | awk 'NR==2 {print $6, $3}')
+DiskUsage=$(echo "$DiskInfo" | cut -d " " -f1)
+DiskCapacity=$(echo "$DiskInfo" | cut -d " " -f2)
 
-DiskPer=$(df -hT | grep -w "/" | perl -p -e 's/ {2,}/ /g' | cut -d " " -f6)
-DiskCapacity=$(df -hT | grep -w "/" | perl -p -e 's/ {2,}/ /g' | cut -d " " -f3)
+# RAM Information
+TotalRam=$(free | awk '/Mem/ {print $2}')
+UsedRam=$(free | awk '/Mem/ {print $3}')
+RAMUsage=$(awk "BEGIN {printf \"%.2f\", ($UsedRam / $TotalRam) * 100}")
 
-#VARIABLES RAM
-TotalRam=$(free | perl -p -e 's/ {2,}/ /g' | grep "Mem" | cut -d " " -f2)
-UsedRam=$(free | perl -p -e 's/ {2,}/ /g' | grep "Mem" | cut -d " " -f3)
-RAM=$(echo "scale=3; ($UsedRam / $TotalRam) * 100" | bc)
+# Swap Information
+TotalSwap=$(free | awk '/Swap/ {print $2}')
+UsedSwap=$(free | awk '/Swap/ {print $3}')
+SwapUsage=$(awk "BEGIN {printf \"%.2f\", ($UsedSwap / $TotalSwap) * 100}")
 
+# Process Information
+TotalProcesses=$(ps -e --no-headers | wc -l)
+RootProcesses=$(ps -u root --no-headers | wc -l)
 
-#VARIABLES SWAP
-TotalSwap=$(free | perl -p -e 's/ {2,}/ /g' | grep "Swap" | cut -d " " -f2)
-UsedSwap=$(free | perl -p -e 's/ {2,}/ /g' | grep "Swap" | cut -d " " -f3)
-Swap=$(echo "scale=3; ($UsedSwap / $TotalSwap) * 100" | bc)
+# IP Information - Join multiple IPs with "-"
+ipv4=$(ip -4 addr show scope global | awk '/inet/ {print $2}' | paste -sd " - ")
+ipv6=$(ip -6 addr show scope global | awk '/inet6/ {print $2}' | paste -sd " - ")
+ipv4=${ipv4:-"No IPv4 Address"}
+ipv6=${ipv6:-"No IPv6 Address"}
 
+# Last System Boot
+lastlog=$(last reboot | head -1 | awk '{print $5, $6, $7, $8, $9}')
 
-#VARIABLES Processes
-Pro=$(ps aux | wc -l )
-Processes=$(echo "scale=3; $Pro - 1" | bc)
-RootProcesses=$(ps aux | cut -d " " -f1 | grep "root" | wc -l)
+# Logged Users
+LoggedUsers=$(who | awk '{print $1}' | sort | uniq | paste -sd ", " -)
 
-#Variables Ipv4 & Ipv6
-ipv4=$(ip -4 addr show | grep -v "127.0.0.1" | grep "inet" | cut -d " " -f6 )
-if [ -z "$ipv4" ]; then
-    ipv4="No IPv4 Address"
-fi
-
-ipv6=$(hostname -I | cut -d " "  -f2)
-if [ -z "$ipv6" ]; then
-    ipv6="No IPv6 Address"
-fi
-#Variables Last Login
-lastlog=$(last down | cut -d " " -f3,4,5,6,7 | tail -1)
-
-# Variables Loged Users
-LoggedUsers=$(who | cut -d " " -f1)
-
-
-# VARIABLES SSH LASTLOGIN
+# Last SSH Connection with improved parsing and date formatting
 ssh_log=$(grep 'sshd' /var/log/auth.log | grep 'Accepted' | tail -1)
 if [ -z "$ssh_log" ]; then
     ssh_info="No SSH Connection"
@@ -53,21 +48,15 @@ else
     ssh_info="The last SSH login for user $sshuser was at $sshtime from $sship port $sshport"
 fi
 
-#Menu
-
-echo "System Information of $(date)"
-echo "                                                   "
-echo "Usage of / :             $DiskPer of $DiskCapacity "
-echo "Memory Usage :           $RAM% "
-echo "Memory Swap :            $Swap%"
-echo "Total Proceeses :        $Processes"
-echo "Root Processes :         $RootProcesses"
-echo "IPv4 address :           $ipv4     "
-echo "IPv6 address :           $ipv6     "
-echo "Last Login :             $lastlog"
-echo "                                                   "
-
+# Display System Information
+echo "Disk Usage (/):              $DiskUsage of $DiskCapacity"
+echo "RAM Usage:                   $RAMUsage%"
+echo "Swap Usage:                  $SwapUsage%"
+echo "Total Processes:             $TotalProcesses"
+echo "Root Processes:              $RootProcesses"
+echo "IPv4 Address(es):            $ipv4"
+echo "IPv6 Address(es):            $ipv6"
+echo "Last System Boot:            $lastlog"
+echo "Logged Users:                $LoggedUsers"
 echo "$ssh_info"
-
-echo "                                   "
-echo "Logged Users :           $LoggedUsers "
+echo "--------------------------------"
